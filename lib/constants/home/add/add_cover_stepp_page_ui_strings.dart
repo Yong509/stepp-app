45c09e4d2257b8ccf264e7bf9ec78d3a79da2e07
yongggnd <yongkerkwith@gmail.com>
2024-03-11 00:06:19 +0700

    remove constant

--- lib/constants/home/add/add_cover_stepp_page_ui_strings.dart
+++ lib/constants/home/add/add_cover_stepp_page_ui_strings.dart
@@ -1,4 +1,6 @@
 class AddCoverSteppUiStrings {
   static const pageTitle = "New STEPP";
   static const preview = "Preview";
+  static const addImageOrVideo = "Add a IMG/VDO";
+  static const addDescriptionOrFriends = "Add Friends/Description";
 }
--- lib/constants/home/add/add_cover_stepp_tab_menu.dart
+++ lib/constants/home/add/add_cover_stepp_tab_menu.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:stepp_app/constants/home/add/add_cover_stepp_page_ui_strings.dart';
 import 'package:stepp_app/widgets/home/add_stepp_place/add_cover_stepp_description.dart';
 import 'package:stepp_app/widgets/home/add_stepp_place/add_cover_stepp_image_tab.dart';
 
 class AddCoverSteppTabMenu {
   static List<Tab> addCoverSteppTabMenu = [
     const Tab(
-      text: "Add a IMG/VDO",
+      text: AddCoverSteppUiStrings.addImageOrVideo,
     ),
     const Tab(
-      text: "Add Friends/Description",
+      text: AddCoverSteppUiStrings.addDescriptionOrFriends,
     ),
   ];
 
